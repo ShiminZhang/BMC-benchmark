f3b6bba245035c0beeb6b19e9fbeba8d94ff2033
s568zhan@uwaterloo.ca <s568zhan@uwaterloo.ca>
2026-06-01 06:46:55 -0700

    update readme/gitignore etc.

--- src/scripts/utils/utils.py
+++ src/scripts/utils/utils.py
@@ -3,18 +3,23 @@ import os
 DEBUG = False
 
 def run_slurm_job_wrap(cmd, output, job_name,wait_id=None,mem="16g", time="20:00:00"):
-    if DEBUG:
-        print(f"Running command: {cmd}")
-        os.system(cmd)
-        return
-    activate_python = "source .env; source $PYENVPATH"
+    # if DEBUG:
+    #     print(f"Running command: {cmd}")
+    #     # os.system(cmd)
+    #     return
+    activate_python = "source ./.env; source $PYENVPATH"
     wrap = f"{activate_python} && {cmd}"
     # os.system(f"sbatch --job-name={job_name} --output={output} --mem={mem} --time={time} --wrap=\"{wrap}\"")
     if wait_id is None: 
         full_cmd = f"sbatch --job-name={job_name} --output={output} --mem={mem} --time={time} --wrap=\"{wrap}\""
     else:
         full_cmd = f"sbatch --dependency=afterok:{wait_id} --job-name={job_name} --output={output} --mem={mem} --time={time} --wrap=\"{wrap}\""
-    # print(full_cmd)
+    
+    if DEBUG:
+        print(full_cmd)
+        # print(f"Running command: {cmd}")
+        # os.system(cmd)
+        return
     job_id = os.popen(full_cmd).read().split()[-1]
     return job_id
 
